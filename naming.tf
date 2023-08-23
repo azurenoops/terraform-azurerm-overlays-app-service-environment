1c8563e54571f43c14ba456c7bb1a865a12d82f0
John Spinella <jrspinella@users.noreply.github.com>
2023-08-23 13:58:05 -0400

    Updates to Naming

--- naming.tf
+++ naming.tf
@@ -8,7 +8,7 @@ data "azurenoopsutils_resource_name" "ase" {
   name          = var.workload_name
   resource_type = "azurerm_app_service_environment"
   prefixes      = [var.org_name, var.use_location_short_name ? module.mod_azregions.location_short : var.location]
-  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.environment, local.name_suffix, var.use_naming ? "" : "ase"])
+  suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "ase"])
   use_slug      = var.use_naming
   clean_input   = true
   separator     = "-"
